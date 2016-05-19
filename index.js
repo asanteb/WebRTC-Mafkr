0e20be68f154b800a07f9be95243964a6c6e2261
Snuggs <rashaunstovall@gmail.com>
2016-05-18 22:54:51 -0400

    Peer JS Integration

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('SPOKANE MAFIA YA BISH!')
+app.use( express.static('public') )
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
+
+// Peer Server
+var peer = require('peer').ExpressPeerServer
+
+var options = {debug: true, port: 3000, path: '/server'}
+
+app.use('/server', peer(server, options))
