121c8dcbc27311fa4c7347d995d3cdaa35b2d40f
Snuggs <rashaunstovall@gmail.com>
2016-05-25 03:40:07 -0400

    Migrate Scripts
      √ [Chore] Update Port
      √ [Chore] Update Package.json
      √ [Chore] Migrate Scripts to public assets

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var express = require('express')
 var app = express()
 
-app.use( express.static('public') )
+  , port = process.env.PORT || 8080
 
-var server = app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port 3000!');
+app
+  .set('port', port)
+  .use( express.static('public') )
+
+var server = app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 })
-
-// Peer Server
-var peer = require('peer').ExpressPeerServer
-
-var options = {debug: true, port: 3000, path: '/server'}
-
-app.use('/server', peer(server, options))
--- serverjs/server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http'); var server = http.createServer(function(req, res) {
-  res.end('Hello from NodeJS!\n');
-  console.log('Someone visited our web server!');
-})
-server.listen(80, '0.0.0.0');
-console.log("NodeJS web server running on 0.0.0.0:8081");
