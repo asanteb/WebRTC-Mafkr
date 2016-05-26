7694179c16bd9abf5f3a8b305961e6f9b075f662
Snuggs <rashaunstovall@gmail.com>
2016-05-26 10:48:06 -0400

    Viewers   Resolves #26
      https://github.com/asantebuil/WebRTC/issues/26
    
      √ Anyone else by default anonymous viewer

--- scripts.js
+++ scripts.js
@@ -1,20 +1,63 @@
-var videos = document.querySelector('#videos');
-var call = document.querySelector('#videos > button');
+window.RMCDefaultChannel = 'coinbabe'
 
-this.disabled = true;
+var DEFAULT_ROOM  = 'spokane'
+  , MODERATOR = 'snuggs'
+  , CHANNEL = 'ABCDEF-' + window.RMCDefaultChannel
+  , SESSION = { video: true, audio: true }
+  , MAX_PARTICIPANTS = 4
 
-window.connection = new RTCMultiConnection();
+  , videos = document.querySelector('#videos')
+  , call = document.querySelector('#videos > button')
+;
+
+window.connection = new RTCMultiConnection(CHANNEL)
+var TOKEN = connection.token()
+connection.maxParticipantsAllowed = MAX_PARTICIPANTS
+
+console.log('CHANNEL: ', CHANNEL)
+console.log('Token: ', TOKEN)
+
+;(function join(room) {
+  if(room) {
+    console.log('Session: ', SESSION)
+    console.log('Joining Room: ', DEFAULT_ROOM)
+
+    connection.join({
+      session: SESSION,
+      userid: MODERATOR,
+      sessionid: DEFAULT_ROOM
+    })
+
+  } else {
+    console.log('User Id: ', connection.userid)
+    console.log('Moderator: ', connection.userid)
+    console.log('Moderating Room: ', DEFAULT_ROOM)
+
+    connection.userid = MODERATOR
+    connection.session = SESSION
+
+    connection.open({
+      dontTransmit: true,
+      sessionid: DEFAULT_ROOM
+    })
+  }
+})(location.hash.slice(1));
+
+connection.onconnected = function(event) {
+  console.log('On Connected: ', arguments)
+}
 
 // dont-override-session allows you force RTCMultiConnection
 // to not override default session of participants;
 // by default, session is always overridden and set to the session coming from moderator!
-connection.dontOverrideSession = true;
+connection.dontOverrideSession = false //true;
 
-connection.session = {
-  audio: true,
-  video: true
-//  oneway: role == 'Anonymous Viewer'
-};
+//connection.session = {
+//  audio: true,
+//  video: true
+////// if there is a session id broadcast anonymously
+////oneway: !connection.isInitiator
+//};
 
 connection.onstream = function(e) {
   videos.appendChild(e.mediaElement);
@@ -24,36 +67,23 @@ connection.onstream = function(e) {
     // initiator will NEVER share participants
     // to manually ask for participants;
     // call "askToShareParticipants" method.
-    connection.askToShareParticipants();
+//  connection.askToShareParticipants();
   }
 
   // if you're moderator
   // if stream-type is 'remote'
   // if target user is broadcaster!
-  if (connection.isInitiator && e.type == 'remote' && !e.session.oneway) {
-    // call "shareParticipants" to manually share participants with all connected users!
-    connection.shareParticipants({
-        dontShareWith: e.userid
-    });
-  }
+//if (connection.isInitiator && e.type == 'remote' && !e.session.oneway) {
+//  // call "shareParticipants" to manually share participants with all connected users!
+//  connection.shareParticipants({
+//      dontShareWith: e.userid
+//  });
+//}
 };
 
 
 connection.onNewSession = function(session) {
-//  if (role == 'Anonymous Viewer') {
-//    session.join({
-//      oneway: true
-//    });
-//  }
-
-//  if (role == 'Broadcaster') {
-//    session.join();
-//  }
+  console.log('New Session: ', session.sessionid)
+//if (!connection.isInitiator)
+//  session.join({ oneway: true })
 };
-
-connection.open({
-  sessionid: connection.channel,
-  captureUserMediaOnDemand: false
-});
-
-//  connection.connect(connection.channel);
