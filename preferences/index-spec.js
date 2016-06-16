c529cbc6c813a63d8e52e8889cee1a42d23835e4
Snuggs <rashaunstovall@gmail.com>
2016-06-16 13:55:36 -0400

    [refactor] Convert Preferences to HTML Imports
      This "auto-registration" routine will import linked web components.
    
      ## Custom Elements
      When providing an id to the import link it will look for an element
      with the respective tag name within the document.
    
      ```HTML5
      <link id='x-foo' rel='import' href='...' />
    
      ...
    
      <x-foo></x-foo>
      ```
    
      ## Custom Element Registration
      The Custom Element will auto-register new component.
      Due to naming conflicts (backwards/forwards compatibility issues)
      Each custom element MUST contain a hyphen within the tagname (i.e. `<x-foo/>`)
    
      ## Web Components Polyfill
        √ Add Webcomponents.js
    
      *Note: Only compatible with Chrome & Firefox without (webcomponents.js)
      webcomponents.js (http://webcomponents.org/polyfills)
    
      ### Compatability table
        - https://github.com/WebComponents/webcomponentsjs#user-content-browser-support
    
      - https://w3c.github.io/webcomponents/spec/custom
      - http://www.html5rocks.com/en/tutorials/webcomponents/customelements
      - https://developer.mozilla.org/en-US/docs/Web/API/Document/registerElement
      - HTML Imports - http://www.html5rocks.com/en/tutorials/webcomponents/imports

---
+++ preferences/index-spec.js
@@ -0,0 +1,11 @@
+require('../spec-helper')
+
+describe('Preferences', function () {
+  describe('video source', function () {
+    it('allows camera source selection')
+
+    context('when revisiting', function () {
+      it('remembers previously selected camera source')
+    })
+  })
+})
